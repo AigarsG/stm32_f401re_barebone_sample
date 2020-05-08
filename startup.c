71fb5eede358811f58841c98b1219717b5260242
Aigars Gridjusko <aigars.gridjusko@gmail.com>
2020-05-08 19:15:06 +0300

    Rework memory layout in linker script which solves section overlay error on native build

--- startup.c
+++ startup.c
@@ -1,13 +1,17 @@
 /* Defined in linker script */
-extern unsigned _DATA_ROM_START;
-extern unsigned _DATA_RAM_START;
-extern unsigned _DATA_RAM_END;
-extern unsigned _BSS_START;
-extern unsigned _BSS_END;
+/* Begin address for the initialisation values of the .data section */
+extern unsigned _sidata;
+/* Begin address for the .data section */
+extern unsigned _sdata;
+/* End address for the .data section */
+extern unsigned _edata;
+/* Start address for the .bss section */
+extern unsigned _sbss;
+/* End address for the .bss section */
+extern unsigned _ebss;
 
 
-#define SRAM_BASE_ADDR 0x20000000
-#define STACK_TOP SRAM_BASE_ADDR + 0x5000
+#define STACK_TOP 0x20018000
 void _start();
 extern int main();
 extern void button_press_handler(void);
@@ -73,29 +77,38 @@ unsigned *stm32_f401re_vectors[] __attribute__ ((section("vectors"))) = {
 	(unsigned *)button_press_handler
 };
 
+
+static void __initialize_data(unsigned *flash_begin, unsigned *data_begin,
+			      unsigned *data_end)
+{
+	unsigned *p = data_begin;
+	while (p < data_end) {
+		*p = *flash_begin;
+		p++;
+		flash_begin++;
+	}
+}
+
+static void __initialize_bss(unsigned *bss_begin, unsigned *bss_end)
+{
+	unsigned *p = bss_begin;
+	while (p < bss_end) {
+		*p = 0;
+		p++;
+	}
+}
+
 void _start()
 {
 	/* 
 	 * Copy data belonging to the '.data' section from its load time
 	 * position on flash (ROM) to its run time position in SRAM
 	 */
-	unsigned *data_rom_start_p = &_DATA_ROM_START;
-	unsigned *data_ram_start_p = &_DATA_RAM_START;
-	unsigned *data_ram_end_p = &_DATA_RAM_END;
-	while (data_ram_start_p < data_ram_end_p) {
-		*data_ram_start_p = *data_rom_start_p;
-		data_ram_start_p++;
-		data_rom_start_p++;
-	}
+	__initialize_data(&_sidata, &_sdata, &_edata);
 
 	/* Initialize data in the '.bss' section to zeros */
-	unsigned *bss_start_p = &_BSS_START;
-	unsigned *bss_end_p = &_BSS_END;
-	while (bss_start_p < bss_end_p) {
-		*bss_start_p = 0;
-		bss_start_p++;
-	}
+	__initialize_bss(&_sbss, &_ebss);
 
-	/* Call the 'main()' function defined in 'main.c' */
+	/* Call the 'main()' function */
 	main();
 }

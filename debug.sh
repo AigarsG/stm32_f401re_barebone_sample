5f309283e78d94f97074d3eb52dafa6e6a8818b3
Aigars Gridjusko <aigars.gridjusko@gmail.com>
2020-11-03 10:55:20 +0200

    Use target extended-remote to allow flashing from gdb

--- debug.sh
+++ debug.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 st-util &
-gdb-multiarch -tui --eval-command="target remote localhost:4242" main.elf
+gdb-multiarch -tui --eval-command="target extended-remote localhost:4242" main.elf
